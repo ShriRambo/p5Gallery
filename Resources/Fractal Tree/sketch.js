635946996d619f9be7822357e93b905cd4741a4c
Shriram <sashirgade+github@gmail.com>
2019-01-28 12:52:08 +0100

    First Draft
    Initial draft of gallery and sketches.

---
+++ Resources/Fractal Tree/sketch.js
@@ -0,0 +1,94 @@
+var angle =0;
+var seed = 0;
+var d = 0.75;
+var Cannvass;
+
+
+var slider;
+
+function resetSketch(){
+    loop();
+}
+
+function setup() {
+    Cannvass = createCanvas(750,410);
+    Cannvass.parent("canvas");
+    noLoop();    
+}
+
+
+function draw() {
+    background(51);
+    seed = seed + 0.01; // slider.value();
+    translate(width/2,height)
+    len = 80;
+    stroke(255);
+    strokeWeight(3);
+    branch(len);
+
+    //noLoop();
+
+}
+
+
+function branch(len) {
+    
+    strokeWeight(3*(len/100));
+    line(0, 0, 0, - len);
+    translate(0,-len);
+    angle = noise(seed + len)
+    randomSeed(len*sqrt(99)*101);
+    r1 = random();
+    r2 = random();
+    if (len > 6){
+        
+        if (r1 > 0.0 ){
+            push();
+            angle = noise(seed + len);
+                rotate(angle);
+                branch( len*d);
+            pop();
+        }
+      
+        if (r2 > 0.0){
+            push();
+            angle = noise(seed + len + 100);
+                rotate(-angle);
+                branch( len*d);
+            pop();
+        }
+    } else{
+        push();
+        fill("#556B2F70");
+        noStroke();
+        ellipse(0, 0, 5, 5);
+        pop();
+    }
+    
+   
+}
+
+
+
+
+
+
+
+
+
+
+function keyPressed() {
+    
+    if(keyCode == DELETE){
+        console.log('Animation Stopped');
+        noLoop();
+       // name = 'TheTreeID' + seedID;
+        //saveCanvas(Cannvass, name, 'jpg');
+    }
+}
+
+
+
+function saveIT(){
+    saveCanvas(Cannvass, "Sketch",'jpg');
+}
---
+++ Resources/Packing circles/sketch.js
@@ -0,0 +1,116 @@
+var circles;
+var Cannvass;
+var maxNC = 500;
+
+
+function resetSketch(){
+    loop();
+    reSetup();
+    
+}
+
+function reSetup(){
+    colorMode(HSB, 100);
+    background("#F2EFE6");
+    circles = [];
+}
+
+
+function setup() {
+    Cannvass = createCanvas(600,400);
+    Cannvass.parent("canvas");
+    reSetup();
+    noLoop();
+}
+
+function draw() {
+    background("#F2EFE6");
+    // noLoop();
+    if(frameCount % 10 == 0 ){
+        if(circles.length < maxNC){
+            circles.push(getCircle());
+        }
+    }
+
+    for(let i = 0; i < circles.length; i++){
+        circles[i].grow();
+        circles[i].show();
+    }
+
+}
+
+function getCircle(){
+    isValid = false;
+    startRad  = 3;
+    while(!isValid){
+        //console.log("Not found");
+        var x = random(10,width-10);
+        var y = random(10,height-10);
+        
+        var i;
+        for (i = 0; i < circles.length; i++ ){
+            circ = circles[i];
+            if(dist(x,y,circ.x,circ.y)   - 5 < circ.radius + startRad){
+                break;
+            }
+        }
+        if(i == circles.length) { isValid = true; }
+        
+    }
+
+    return new Circle(x,y);
+}
+
+function Circle(x,y){
+    this.x = x;//random(10,width-10);
+    this.y = y;//random(10,height-10);
+    this.radius = 2;
+    this.growing = true;
+    this.col = color(random(100),random(50),random(50,100));
+
+
+    this.grow = function(){
+       if(this.growing){
+            minx = min(this.x, width - this.x)
+            miny = min(this.y,height-this.y);
+            
+            // Check if touching boundary
+            if( minx -5 <= this.radius || miny -5 <= this.radius){
+                this.growing = false;
+                //console.log("Touched boundary");
+                //break;
+            }else {
+                // Check the distance 
+                for(let i = 0; i < circles.length;i++){
+                    circ = circles[i];
+                    if(this == circ){continue;}
+                    if(dist(this.x,this.y,circ.x,circ.y) - 3 <= this.radius + circ.radius ){
+                       // console.log("Touched a circle");
+                        this.growing = false;
+                        break;
+                    }
+                }
+            }
+
+        }
+
+
+        if(this.growing) { this.radius += 2; }
+    }
+
+
+    this.show = function(){
+        fill(this.col);
+        noStroke(0);
+        //strokeWeight(3);
+        ellipse(this.x,this.y,2*this.radius,2*this.radius);
+    }
+
+}
+
+
+
+
+function saveIT(){
+    saveCanvas(Cannvass, "Sketch",'jpg');
+}
